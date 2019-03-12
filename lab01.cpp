0a87be93ab6da57ec0b77daa4500cf3dd626a797
SimonRussia <git.streisand@gmail.com>
2019-03-12 23:16:14 +0300

    Update lab01.cpp

--- lab01.cpp
+++ lab01.cpp
@@ -3,6 +3,45 @@
 #include <bitset>
 
 // Функция составления двоичных комбинаций 2^4
+void binary(std::vector<std::vector<double> > & xt);
+
+// Функция построения таблицы истинности.
+void true_foo(std::vector<std::vector<double> > & xt, std::vector<double> & yt);
+
+void print_y(std::vector<std::vector<double> > const & xt, std::vector<double> const & yt);
+
+
+int main(int argc, char const *argv[])
+{
+
+	double delta = 0;
+	double nu = 0.3;
+	double dFoo = 1;
+	
+	// вектор комбинаций x1 x2 x3 x4 (двумерный вектор) (16) (x0 = 1) (x[@][i] = Xi)
+	std::vector<std::vector<double> > x (16);
+	binary(x);
+
+
+	// вектор истинных значений y (двумерный вектор) (16)
+	std::vector<double> true_y;
+	true_foo(x, true_y);
+
+	print_y(x, true_y);
+
+	// вектор весовых коэффициентов (5) W0 = w[4]
+	std::vector<double> w (5, 0);
+
+	// Выходной вектор функциии (2^4)
+	std::vector<double> y;
+
+	// std::cout << "> > > main - Ok!" << std::endl;
+
+
+	return 0;
+}
+
+// Функция составления двоичных комбинаций 2^4
 void binary(std::vector<std::vector<double> > & xt)
 {
 	for (int i = 0; i < 16; i++)
@@ -52,44 +91,3 @@ void print_y(std::vector<std::vector<double> > const & xt, std::vector<double> c
 
 	// std::cout << "> > > print_y - Ok!" << std::endl;
 }
-
-
-int main(int argc, char const *argv[])
-{
-	
-	// вектор комбинаций x1 x2 x3 x4 (двумерный вектор) (16) (x0 = 1) (x[@][i] = Xi)
-	std::vector<std::vector<double> > x (16);
-	binary(x);
-
-	// std::cout << x.size() << " " << x[9].size() << " value - " << x[13][3] << std::endl;
-
-
-	// вектор истинных значений y (двумерный вектор) (16)
-	std::vector<double> true_y;
-	true_foo(x, true_y);
-
-	print_y(x, true_y);
-
-	// вектор весовых коэффициентов (5)
-	std::vector<double> w;
-
-	// Выходной вектор функциии (2^4)
-	std::vector<double> y;
-
-	// std::cout << "> > > main - Ok!" << std::endl;
-
-
-	return 0;
-}
-
-
-
-
-/*
-void add (int n, std::vector<int>& vec){
-    vec.push_back(n);
-}
-
-std::vector <int> myVec;
-add(5, myVec);
-*/
