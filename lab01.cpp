bfe54fda1752b14473e2ae66558c55619a47f51d
SimonRussia <git.streisand@gmail.com>
2019-03-12 22:54:46 +0300

    Update lab01.cpp| truth table function

--- lab01.cpp
+++ lab01.cpp
@@ -1,42 +1,56 @@
-// #include "merge.hpp"
-// #include "message.hpp"
-// #include <random>
-// #include <iterator>
-
-#include <bitset>
 #include <iostream>
 #include <vector>
+#include <bitset>
 
-
-
-
-// функция составления двоичных комбинаций 2^4
+// Функция составления двоичных комбинаций 2^4
 void binary(std::vector<std::vector<double> > & xt)
 {
 	for (int i = 0; i < 16; i++)
 	{
 		std::bitset<4> bin(i);
 
-		// for (int j = 3; j >= 0; j--)
-		// {
-		// 	std::cout << bin[j] << " ";
-		// 	xt[i].push_back( bin[j] );
-		// }
-
 		for (int j = 0; j < 4; j++)
 		{
-			std::cout << bin[j] << " ";
 			xt[i].push_back( bin[j] );
 		}
-
-		std::cout << std::endl;
 	}
+
+	// std::cout << "> > > binary_foo - Ok!" << std::endl;
 }
 
-// расчет функции y 
-void true_foo(std::vector<std::vector<double> > & xt, std::vector<double> & yt;)
+// Функция построения таблицы истинности.
+void true_foo(std::vector<std::vector<double> > & xt, std::vector<double> & yt)
 {
-	ddd
+	for (int i = 0; i < 16; i++)
+	{
+		if ( (xt[i][3]==0) && ( ( (xt[i][0]==0) && (xt[i][1]==0) ) || (xt[i][2]==0) ) ) {
+			yt.push_back(1);
+		}
+		else
+		{
+			yt.push_back (0);
+		}
+	}
+
+	// std::cout << "> > > true_foo - Ok!" << std::endl;
+}
+
+void print_y(std::vector<std::vector<double> > const & xt, std::vector<double> const & yt)
+{
+	std::cout << std::endl << "X4 X3 X2 X1 | Y" << std::endl;
+
+	for (int i = 0; i < 16; i++)
+	{
+		for (int j = 3; j >= 0; j--)
+		{
+			std::cout << " " << xt[i][j] << " ";
+		}
+		std::cout << "| " << yt[i] << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	// std::cout << "> > > print_y - Ok!" << std::endl;
 }
 
 
@@ -46,10 +60,15 @@ int main(int argc, char const *argv[])
 	// вектор комбинаций x1 x2 x3 x4 (двумерный вектор) (16) (x0 = 1) (x[@][i] = Xi)
 	std::vector<std::vector<double> > x (16);
 	binary(x);
-	// std::cout << std::endl << x[14][3] << " " << x[14][2] << " " << x[14][1] << " " << x[14][0] << " " << std::endl;
+
+	// std::cout << x.size() << " " << x[9].size() << " value - " << x[13][3] << std::endl;
+
 
 	// вектор истинных значений y (двумерный вектор) (16)
 	std::vector<double> true_y;
+	true_foo(x, true_y);
+
+	print_y(x, true_y);
 
 	// вектор весовых коэффициентов (5)
 	std::vector<double> w;
@@ -57,7 +76,7 @@ int main(int argc, char const *argv[])
 	// Выходной вектор функциии (2^4)
 	std::vector<double> y;
 
-	std::cout << "******* OK *******" << std::endl;
+	// std::cout << "> > > main - Ok!" << std::endl;
 
 
 	return 0;
